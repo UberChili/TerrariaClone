2b8e91e2f2917783860b95bece927cda7c4eb5c5
Juan Andres Nuñez <nunezlopez.ja@gmail.com>
2025-08-12 17:12:31 -0600

    Unknown modification to main.c Modified Makefile
    I copied the structure of the Makefile from the filter
    project (unfinished) in order to use the same "flexibility" (?)

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 # Name of the output binary
 NAME = terraria
 
-# Source and output dirs
-SRC = src/main.c
-BIN = bin/$(NAME)
+CC = gcc
+# CFLAGS = -Wall -Wextra -Werror -Iinclude
+SRCDIR = src
+INCDIR = include
+BUILDDIR = build
+BINDIR = bin
 
 # Detect platform
 UNAME_S := $(shell uname -s)
@@ -15,22 +18,39 @@ ifeq ($(UNAME_S), Darwin)
               -framework OpenGL -framework Cocoa -framework IOKit -framework CoreAudio -framework CoreVideo
 else
     PLATFORM = linux
-    CFLAGS = -Wall -O2
+    CFLAGS = -Wall -O2 -Werror -Wextra
     LDFLAGS = -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 endif
 
-# Compiler
-CC = gcc
+# Object files (all go to build directory)
+# This example was taken from filter, leaving for reference in case I forget
+# OBJS = $(BUILDDIR)/main.o $(BUILDDIR)/helpers.o $(BUILDDIR)/png.o $(BUILDDIR)/chunk.o
+OBJS = $(BUILDDIR)/main.o
 
-# Default target
-all: $(BIN)
+# Main target
+$(BINDIR)/terraria: $(OBJS) | $(BINDIR)
+	$(CC) $(OBJS) $(CFLAGS) $(LDFLAGS) -o $@
 
-$(BIN): $(SRC)
-	@mkdir -p bin
-	$(CC) $(CFLAGS) $(SRC) -o $(BIN) $(LDFLAGS)
+# Compile main.c
+# This example was compiling with filter.h
+# $(BUILDDIR)/main.o: $(SRCDIR)/main.c $(INCDIR)/filter.h | $(BUILDDIR)
+$(BUILDDIR)/main.o: $(SRCDIR)/main.c
+	$(CC) $(CFLAGS) -c $< -o $@
 
+# Compile png.c
+# This example was taken from filter, we don't need it, keeping it for future reference
+# $(BUILDDIR)/png.o: $(SRCDIR)/png.c $(INCDIR)/filter.h | $(BUILDDIR)
+
+# Create directories if they don't exist
+$(BUILDDIR):
+	mkdir -p $(BUILDDIR)
+
+$(BINDIR):
+	mkdir -p $(BINDIR)
+
+# Clean build artifacts
 clean:
-	rm -f $(BIN)
+	rm -f $(BUILDDIR) $(BINDIR)
 
 print-platform:
 	@echo Detected platform: $(PLATFORM)
--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "../lib/raylib.h"
+#include <stdio.h>
 
 #define PLAYER_HDR_SPD 200.0f
 #define PLAYER_JUMP_SPD 350.0f
@@ -17,20 +17,30 @@ typedef struct {
 } EnvItem;
 
 /* void updatePlayer(Player *player, float delta) { */
-void updatePlayer(Player *player, EnvItem envItems[], size_t envItemsLen, float delta) {
-    if (IsKeyDown(KEY_LEFT)) player->position.x -= PLAYER_HDR_SPD * delta;
-    if (IsKeyDown(KEY_RIGHT)) player->position.x += PLAYER_HDR_SPD * delta;
+void updatePlayer(Player *player, EnvItem envItems[], size_t envItemsLen,
+                  float delta) {
+    if (IsKeyDown(KEY_LEFT))
+        player->position.x -= PLAYER_HDR_SPD * delta;
+    if (IsKeyDown(KEY_RIGHT))
+        player->position.x += PLAYER_HDR_SPD * delta;
 
     if (IsKeyDown(KEY_SPACE) && player->canJump) {
-        player->speed = -PLAYER_JUMP_SPD;
+        printf("Jump key pressed!\n");
+        player->speed = -PLAYER_JUMP_SPD * delta;
         player->canJump = false;
     }
 
-    /* for (size_t i = 0; i < envItemsLen; i++ ) { */
-    /*     if (CheckCollisionRecs(envItems[i].rect, )) */
-    /* } */
+    for (size_t i = 0; i < envItemsLen; i++) {
+        if (CheckCollisionRecs(
+                envItems[i].rect,
+                (Rectangle){player->position.x, player->position.y})) {
+            printf("Some rectangles are touching!\n");
+            player->position.y = player->position.y - 3;
+            player->canJump = true;
+        }
+    }
 
-    /* player->position.y += PLAYER_HDR_SPD * delta; */
+    player->position.y += PLAYER_HDR_SPD * delta;
 }
 
 int main(void) {
@@ -40,10 +50,12 @@ int main(void) {
     InitWindow(screenWidth, screenHeight, "Raylib Window");
 
     EnvItem envitems[] = {
-    {{200, screenHeight - 100, screenWidth - 400, 100}, 0, LIGHTGRAY}, // surface
+        {{200, screenHeight - 100, screenWidth - 400, 100},
+         0,
+         LIGHTGRAY}, // surface
     };
 
-    size_t EnvItemsLen = sizeof (envitems) / sizeof(envitems[0]);
+    size_t EnvItemsLen = sizeof(envitems) / sizeof(envitems[0]);
 
     Player player = {{500, screenHeight - 100}, 2, false};
 
@@ -62,9 +74,13 @@ int main(void) {
         }
 
         // Draw Rectangle first
-        Rectangle playerRect = {player.position.x - 20, player.position.y - 40, 40.0f, 40.0f};
+        Rectangle playerRect = {player.position.x - 20, player.position.y - 40,
+                                40.0f, 40.0f};
+        /* Rectangle playerRect = {player.position.x, player.position.y, 40.0f,
+         */
+        /*                         40.0f}; */
 
-        updatePlayer(&player, deltaTime);
+        updatePlayer(&player, envitems, EnvItemsLen, deltaTime);
         DrawRectangleRec(playerRect, YELLOW);
 
         EndDrawing();
